18439206d94d799baf789f10a823c1b6ec4d989c
jir88 <6749336+jir88@users.noreply.github.com>
2023-11-08 11:32:58 -0600

    Cosmetic changes to getPeak

--- R/getPeak.R
+++ R/getPeak.R
@@ -1,10 +1,10 @@
 ## This is the function to get the peaks for all runs
 
-getPeak <- function(Run = list(), file.name = character(), Targets = list(), 
-                    target.file.name = character(), drt = 10/60, dsc = 14/2, 
-                    weight = 2/3, deltaRI = 20, calibRI = NULL, 
+getPeak <- function(Run = list(), file.name = character(), Targets = list(),
+                    target.file.name = character(), drt = 10/60, dsc = 14/2,
+                    weight = 2/3, deltaRI = 20, calibRI = NULL,
                     rt.sort = FALSE) {
-    
+
     ## Initialization
     # load the target info from file, if Targets is not provided
     if (missing(Targets)) {
@@ -12,9 +12,9 @@ getPeak <- function(Run = list(), file.name = character(), Targets = list(),
         if (missing(target.file.name)) {
             stop("When \"Target\" is not provided, \"target.file.name\" is required!")
         }
-        Targets <- readRDS(target.file.name)    
+        Targets <- readRDS(target.file.name)
     }
-        
+
     compound <- Targets$compound
     rt <- Targets$rt
     ms <- Targets$ms
@@ -22,7 +22,7 @@ getPeak <- function(Run = list(), file.name = character(), Targets = list(),
     ri <- Targets$ri
     quantFrag <- Targets$quantFrag
     num.compound <- length(compound)
-            
+
     # sort based on RTs, if asked by user
     if (rt.sort) {
         rt <- sort.int(rt, index.return = TRUE)$x
@@ -33,81 +33,81 @@ getPeak <- function(Run = list(), file.name = character(), Targets = list(),
         quantFrag <- quantFrag[ind]
         compound <- compound[ind]
     }
-    
+
     # sort fragments based on quantifier mass
     for (j in 1:num.compound) {
         num.frags <- length(ms[[j]])
-        indFrag <- seq(1, num.frags)    
+        indFrag <- seq(1, num.frags)
         indFrag <- c(quantFrag[j], indFrag[indFrag != quantFrag[j]])
         ms[[j]] <- ms[[j]][indFrag]
         sp[[j]] <- sp[[j]][indFrag]
     }
-    
+
     if (!is.list(ms)) {
         ms <- list(ms)
         sp <- list(sp)
     }
-    
-    
-    
+
+
+
     ## get EIC data of each run per target
 
-    # check if a single run is provided, if not, the file.name should be 
-    # provided which includes all the file names of the runs to be processed, 
-    # otherwise the peaks of a single run provided by Run variable is retreived
+    # check if a single run is provided, if not, the file.name should be
+    # provided which includes all the file names of the runs to be processed,
+    # otherwise the peaks of a single run provided by Run variable is retrieved
     if (missing(Run)) {
         # get the number of runs
         if (missing(file.name)) {
-            stop("When \"Run\" is not provided, the \"file.name\" is required!")    
-        }            
-        num.run <- length(file.name) 
-        
+            stop("When \"Run\" is not provided, the \"file.name\" is required!")
+        }
+        num.run <- length(file.name)
+
         # initialize the output variable
         dsPeaks <- list()
-      
+
         for (i in 1:num.run) {
             # initialize
             runPeaks <- list()
-            
+
             # load related RData file for each run
             Run <- readRDS(file = paste(file.name[i], '.rds', sep = ""))
-        
+
             # call getEIC to find the EIC of each target
             for (j in 1:num.compound) {
-                peakEIC <- getEIC(Run = Run, compound = compound[j], 
-                                  ms0 = ms[[j]], sp0 = sp[[j]], rt0 = rt[j], 
-                                  drt = drt, dsc = dsc, ri0 = ri[j], 
+                peakEIC <- getEIC(Run = Run, compound = compound[j],
+                                  ms0 = ms[[j]], sp0 = sp[[j]], rt0 = rt[j],
+                                  drt = drt, dsc = dsc, ri0 = ri[j],
                                   weight = weight, deltaRI = deltaRI,
                                   calibRI = calibRI)
                 runPeaks[j] <- list(peakEIC)
             }
-        
+
             dsPeaks[i] <- list(runPeaks)
-            
+
             cat(paste('\n\t', file.name[i], 'done.\n', sep = ' '))
         }
-        
+
         names(dsPeaks) <- file.name
 
     } else {
         # call getEIC to find the EIC of each target
         runPeaks <- list()
-        
+
         for (j in 1:num.compound) {
-            peakEIC <- getEIC(Run = Run, compound = compound[j], 
-                              ms0 = ms[[j]], sp0 = sp[[j]], rt0 = rt[j], 
-                              drt = drt, dsc = dsc, ri0 = ri[j], 
-                              weight = weight, deltaRI = deltaRI, 
+            peakEIC <- getEIC(Run = Run, compound = compound[j],
+                              ms0 = ms[[j]], sp0 = sp[[j]], rt0 = rt[j],
+                              drt = drt, dsc = dsc, ri0 = ri[j],
+                              weight = weight, deltaRI = deltaRI,
                               calibRI = calibRI)
             runPeaks[j] <- list(peakEIC)
         }
-        
+
         dsPeaks <- list(runPeaks)
         names(dsPeaks) <- Run$file.name
     }
 
-    ## output the results 
+    ## output the results
     # saveRDS(dsPeaks, file = "dsPeaks.rds", compress = "xz")
     return(dsPeaks)
-    
+
 }
